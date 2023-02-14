91bf71b93b26a13c2f48aa1cbd09209ea491dfa8
joer80 <joer80@gmail.com>
2023-02-14 09:04:52 -0600

    ini

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import Alpine from 'alpinejs'
+import focus from '@alpinejs/focus'
+import FormsAlpinePlugin from '../../vendor/filament/forms/dist/module.esm'
+import NotificationsAlpinePlugin from '../../vendor/filament/notifications/dist/module.esm'
+
+Alpine.plugin(focus)
+Alpine.plugin(FormsAlpinePlugin)
+Alpine.plugin(NotificationsAlpinePlugin)
+
+window.Alpine = Alpine
+
+Alpine.start()
+
