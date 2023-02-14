73b388051f6a23a061f44121f6ace487e305cb5c
joer80 <joer80@gmail.com>
2023-02-14 10:50:03 -0600

    Allow root user to use filament in production

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,9 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +43,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function canAccessFilament(): bool
+    {
+        //id of user that can use admin
+        if ($this->id == 1) {
+            $canSee = 1;
+        } else {
+            $canSee = 0;
+        }
+
+        return $canSee;
+    }
 }
